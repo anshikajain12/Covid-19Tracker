030d5e5f883c452cea0f9aea7901eaaab33803d5
Anshika Jain <anshikajain@ingenuitygaming.com>
2022-08-26 16:00:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,62 @@
+const API_URL = "https://api.covid19api.com/summary";
+
+function getApiData(url) {
+  return new Promise((resolve, reject) => {
+    var request = new XMLHttpRequest();
+    request.open("GET", url);
+    request.send();
+
+    request.onload = () => {
+      //   console.log(request);
+
+      if (request.status === 200) {
+        resolve(request.response);
+      } else {
+        reject("Not able to fetch Data");
+      }
+    };
+  });
+}
+
+getApiData(API_URL)
+  .then((val) => {
+
+    displayData(JSON.parse(val));
+  })
+  .catch((err) => console.log("Error" + err));
+
+function displayData(serverData) {
+
+  var s = document.getElementById("select");
+
+  for (var i = 0; i < serverData.Countries.length; i++) {
+    var o = document.createElement("option");
+    o.text = serverData.Countries[i].Country;
+    o.value = i;
+    s.appendChild(o);
+  }
+  var x = document.querySelector("form").appendChild(s);
+
+  s.onclick = function () {
+    if (s.options[s.selectedIndex].value==="Country Name") {
+      document.getElementById("y2").innerHTML = "";
+      document.getElementById("y3").innerHTML = "";
+      document.getElementById("y4").innerHTML = "";
+      document.getElementById("y5").innerHTML = "";
+      document.getElementById("y6").innerHTML = "";
+    } else {
+      var checkvalue = s.options[s.selectedIndex].value;
+      document.getElementById("y2").innerHTML =serverData.Countries[checkvalue].Date;
+      document.getElementById("y3").innerHTML =serverData.Countries[checkvalue].NewDeaths;
+      document.getElementById("y4").innerHTML =serverData.Countries[checkvalue].TotalConfirmed;
+      document.getElementById("y5").innerHTML =serverData.Countries[checkvalue].TotalDeaths;
+      document.getElementById("y6").innerHTML =serverData.Countries[checkvalue].NewConfirmed;
+    }
+    setTimeout(()=>{
+      var audio = new Audio("./audio/covid19.mp3");
+      audio.play();
+    },2000);
+  };
+ 
+
+}
